84797020e118c2ed35fb787657d000e325c27c8a
missiona.carla <missiona.carla@gmail.com>
2022-04-03 14:23:07 +0800

    Query
    Query

--- src/index.ts
+++ src/index.ts
@@ -14,15 +14,15 @@ app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 app.use(bodyParser.text({ type: "text/html" }));
 
 app.get("/blockpulse/users", async (req, res) => {
-  const { rows } = await pool.query("SELECT * FROM BLOCKCHAIN_PULSE_USER");
+  const { q } = await pool.query("SELECT * FROM BLOCKCHAIN_PULSE_USER");
   // res.send(`Hello, World! The time from the DB `);
-  console.log(rows.rows);
+  console.log(q.rows);
   res.json({
     data: [
       {
         quote: 'Not invited',
         author: 'Invader',
-        data: rows.rows
+        data: q.rows
       }
     ],
     meta: {
