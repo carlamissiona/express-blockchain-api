aef0fea9be2b7e125db08dfda08356505c9fa75c
missiona.carla <missiona.carla@gmail.com>
2022-04-03 14:25:36 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -16,13 +16,13 @@ app.use(bodyParser.text({ type: "text/html" }));
 app.get("/blockpulse/users", async (req, res) => {
   const { q } = await pool.query("SELECT * FROM BLOCKCHAIN_PULSE_USER");
   // res.send(`Hello, World! The time from the DB `);
-  console.log(q.rows);
+  console.log(q);
   res.json({
     data: [
       {
         quote: 'Not invited',
         author: 'Invader',
-        data: q.rows
+        data: q
       }
     ],
     meta: {
