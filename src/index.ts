9da9d938e87efa96f075a9534c1085eb3a73a8e3
missiona.carla <missiona.carla@gmail.com>
2022-04-03 14:45:28 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,7 @@ app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 app.use(bodyParser.text({ type: "text/html" }));
 
 app.get("/blockpulse/users", async (req, res) => {
-  const { rows } = await pool.query("SELECT * FROM BLOCKCHAIN_PULSE_USER");
-   
+  const { rows } = await pool.query("SELECT * FROM BLOCKCHAIN_PULSE_USER");   
   console.log(rows);
   res.json({
     data: [
@@ -29,6 +28,39 @@ app.get("/blockpulse/users", async (req, res) => {
   });
   
 });
+
+app.get("/blockpulse/prices", async (req, res) => {
+  const { rows } = await pool.query("SELECT * FROM BLOCKCHAIN_PULSE_PRICES");   
+  console.log(rows);
+  res.json({
+    data: [
+      {
+        prices: rows
+      }
+    ],
+    meta: {
+      page: 'na'
+    }
+  });
+  
+});
+
+app.post("/blockpulse/prices/create", async (req, res) => {
+  const { rows } = await pool.query("INSERT INTO BLOCKCHAIN_PULSE_PRICES ('name',	'source',	'price', 'type') ($1, $2, $3, $4)', [name,source,price, type], )");   
+  console.log(rows);
+  res.json({
+    data: [
+      {
+        prices: rows
+      }
+    ],
+    meta: {
+      page: 'na'
+    }
+  });
+  
+});
+id	name	source	price	date_added	date_modified	type
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
