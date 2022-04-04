5af35373d3c88574e62878d90368028d46e62a8d
missiona.carla <missiona.carla@gmail.com>
2022-04-04 22:14:08 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import bodyParser from "body-parser";
-import express from "express";
+import express from "expresszz";
 import pg from "pg";   
 
 // Connect to the database using the DATABASE_URL environment
